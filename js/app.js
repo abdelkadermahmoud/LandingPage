e0c12969c8721be353fc90c95ff20bcca7e70030
abdelkadermahmoud <30759354+abdelkadermahmoud@users.noreply.github.com>
2021-07-03 22:55:38 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -25,14 +25,24 @@ const sections = document.querySelectorAll("section");
  * Start Helper Functions
  * 
 */
-function isInViewPort(section){
-         section=section.getBoundingClientRect();
-        return(
-            section.left>=0&&
-            section.top>=0&&
-            section.bottom<=(document.documentElement.clientHeight)&&
-            section.right<=(document.documentElement.clientWidth)
-        )
+function isInViewPort(el){
+    var top = el.offsetTop;
+    var left = el.offsetLeft;
+    var width = el.offsetWidth;
+    var height = el.offsetHeight;
+  
+    while(el.offsetParent) {
+      el = el.offsetParent;
+      top += el.offsetTop;
+      left += el.offsetLeft;
+    }
+  
+    return (
+      top >= window.pageYOffset &&
+      left >= window.pageXOffset &&
+      (top + height) <= (window.pageYOffset + window.innerHeight) &&
+      (left + width) <= (window.pageXOffset + window.innerWidth)
+    );
 }
 
 /**
